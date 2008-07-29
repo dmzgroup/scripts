23a1a71d3a36a6a96f3d286d9f76ad2ae2d0f04b
Randall E. Barker <simstorm@mac.com>
2008-07-29 12:42:36 -0700

    adding to utility scripts

---
+++ gpull.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+GITREPO="$HOME/cm/src"
+DIRS="`/bin/ls -1 $GITREPO`"
+for dir in $DIRS ; do
+   if [ -d $GITREPO/$dir/.git ] ; then
+      echo "#dir -> git pull"
+      cd $GITREPO/$dir ; git pull
+      echo
+   fi
+done
---
+++ gstatus.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+GITREPO="$HOME/cm/src"
+DIRS="`/bin/ls -1 $GITREPO`"
+for dir in $DIRS ; do
+   if [ -d $GITREPO/$dir/.git ] ; then
+      echo "#dir -> git status"
+      cd $GITREPO/$dir ; git status
+      echo
+   fi
+done
