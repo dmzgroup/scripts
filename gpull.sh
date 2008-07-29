1fa79b9ec7869a85e64f37c16f55ba16cdc8b8a0
Randall E. Barker <simstorm@mac.com>
2008-07-29 12:57:56 -0700

    fix echo messaged to show actual directory

--- gpull.sh
+++ gpull.sh
@@ -3,7 +3,7 @@ GITREPO="$HOME/cm/src"
 DIRS="`/bin/ls -1 $GITREPO`"
 for dir in $DIRS ; do
    if [ -d $GITREPO/$dir/.git ] ; then
-      echo "#dir -> git pull"
+      echo "$dir -> git pull"
       cd $GITREPO/$dir ; git pull
       echo
    fi
--- gstatus.sh
+++ gstatus.sh
@@ -3,7 +3,7 @@ GITREPO="$HOME/cm/src"
 DIRS="`/bin/ls -1 $GITREPO`"
 for dir in $DIRS ; do
    if [ -d $GITREPO/$dir/.git ] ; then
-      echo "#dir -> git status"
+      echo "$dir -> git status"
       cd $GITREPO/$dir ; git status
       echo
    fi
