1854802c005c0f8980bc794284c20994bb0e1a51
Randall E. Barker <simstorm@mac.com>
2008-08-06 17:46:19 -0700

    fixed small typo in script

--- gall.sh
+++ gall.sh
@@ -5,7 +5,7 @@ if [ ! "$1" = "" ] ; then
    DIRS="`/bin/ls -1 $GITREPO`"
    for dir in $DIRS ; do
       if [ -d $GITREPO/$dir/.git ] ; then
-         echo "$dir -> git $1"
+         echo "$dir -> git $*"
          cd $GITREPO/$dir ; git $*
          echo
       fi
