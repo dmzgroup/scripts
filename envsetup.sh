347fca57c84ac99c388f6679832dcbe0f0e7b189
Randall E. Barker <simstorm@mac.com>
2008-12-08 17:26:05 -0800

    Updated to support DMZ_DEBUG env variable.

--- envsetup.sh
+++ envsetup.sh
@@ -3,12 +3,20 @@
 if [ `uname` = "Darwin" ] ; then
    export DYLD_LIBRARY_PATH=../../depend/osg/lib:$DYLD_LIBRARY_PATH
    export BIN_HOME=../../bin/macos-debug ;
+   export DEBUG_EXE="gdb --args "
 elif [ `uname` = "Linux" ] ; then
    export BIN_HOME=../../bin/linux-debug ;
+   export DEBUG_EXE="gdb --args "
 elif [ `uname -o` = "Cygwin" ] ; then
    export BIN_HOME=../../bin/win32-debug ;
+   export DEBUG_EXE="devenv /debugexe "
 else
    echo "Unsupported platform: " `uname`
    exit -1 ;
 fi
 
+if [ "$DMZ_DEBUG" = "true" ] ; then
+   export RUN_DEBUG=$DEBUG_EXE
+else
+   export RUN_DEBUG=""
+fi
