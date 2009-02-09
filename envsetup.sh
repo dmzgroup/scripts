558c809f3b6d26681df18afc2d8baae9cf8e013b
Randall E. Barker <simstorm@mac.com>
2009-02-09 14:18:45 -0800

    envsetup will now add dynamic library path.

--- envsetup.sh
+++ envsetup.sh
@@ -1,14 +1,19 @@
 #!/bin/sh
 
+if [ "$DMZ_BIN_MODE" = "" ] ; then
+   DMZ_BIN_MODE="debug"
+fi
+
 if [ `uname` = "Darwin" ] ; then
-   export DYLD_LIBRARY_PATH=../../depend/osg/lib:$DYLD_LIBRARY_PATH
-   export BIN_HOME=../../bin/macos-debug ;
+   export DYLD_LIBRARY_PATH=../../bin/macos-$DMZ_BIN_MODE:../../depend/osg/lib:$DYLD_LIBRARY_PATH
+   export BIN_HOME=../../bin/macos-$DMZ_BIN_MODE ;
    export DEBUG_EXE="gdb --args "
 elif [ `uname` = "Linux" ] ; then
-   export BIN_HOME=../../bin/linux-debug ;
+   export LD_LIBRARY_PATH=../../bin/macos-$DMZ_BIN_MODE:$LD_LIBRARY_PATH
+   export BIN_HOME=../../bin/linux-$DMZ_BIN_MODE ;
    export DEBUG_EXE="gdb --args "
 elif [ `uname -o` = "Cygwin" ] ; then
-   export BIN_HOME=../../bin/win32-debug ;
+   export BIN_HOME=../../bin/win32-$DMZ_BIN_MODE ;
    export DEBUG_EXE="devenv /debugexe "
 else
    echo "Unsupported platform: " `uname`
