0b287d02cabaa211b5d0542e1aac0270cb5c2b33
Randall E. Barker <simstorm@mac.com>
2009-04-22 12:46:44 -0700

    test this

--- gitbatch.sh
+++ gitbatch.sh
@@ -25,7 +25,7 @@ if [ ! "$1" = "" ] ; then
       for dir in $DIRS ; do
 
          if [ -d $GITREPO/$dir/.git ] ; then
-            echo "$dir -> git $*"
+            echo "$dir -> git" "$@"
             cd $GITREPO/$dir ; git "$@"
             echo
          fi
