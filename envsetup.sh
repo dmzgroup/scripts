97d7b8f60df7df0b92dd79346a483663847950e1
Randall E. Barker <simstorm@mac.com>
2009-05-27 15:34:22 +0200

    Fixed script to that it can be used at any depth instead of hard coded to ../..

--- envsetup.sh
+++ envsetup.sh
@@ -1,19 +1,42 @@
 #!/bin/sh
 
+START_DIR=`pwd`
+
+while [ "`pwd`" != "/" -a "$DMZ_ROOT" = "" ] ; do
+    if [ -d ./lmkproject -a -d ./bin ] ; then
+       DMZ_ROOT=`pwd`
+    else
+       cd ..
+    fi
+done
+
+if [ "$DMZ_ROOT" = "" -a -d "$HOME/cm/" ] ; then
+   DMZ_ROOT="$HOME/cm"
+fi
+
+if [ "$DMZ_ROOT" = ""] ; then
+   echo "Unable to locate the DMZ root directory"
+   exit -1
+else
+   echo "Running from: $DMZ_ROOT"
+   echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
+   cd $START_DIR
+fi
+
 if [ "$DMZ_BIN_MODE" = "" ] ; then
    DMZ_BIN_MODE="debug"
 fi
 
 if [ `uname` = "Darwin" ] ; then
-   export DYLD_LIBRARY_PATH=../../bin/macos-$DMZ_BIN_MODE:../../depend/osg/lib:$DYLD_LIBRARY_PATH
-   export BIN_HOME=../../bin/macos-$DMZ_BIN_MODE ;
+   export DYLD_LIBRARY_PATH=$DMZ_ROOT/bin/macos-$DMZ_BIN_MODE:$DMZ_ROOT/depend/osg/lib:$DMZ_ROOT/depend/v8/lib:$DYLD_LIBRARY_PATH
+   export BIN_HOME=$DMZ_ROOT/bin/macos-$DMZ_BIN_MODE ;
    export DEBUG_EXE="gdb --args "
 elif [ `uname` = "Linux" ] ; then
-   export LD_LIBRARY_PATH=../../bin/linux-$DMZ_BIN_MODE:$LD_LIBRARY_PATH
-   export BIN_HOME=../../bin/linux-$DMZ_BIN_MODE ;
+   export LD_LIBRARY_PATH=$DMZ_ROOT/bin/linux-$DMZ_BIN_MODE:$LD_LIBRARY_PATH
+   export BIN_HOME=$DMZ_ROOT/bin/linux-$DMZ_BIN_MODE ;
    export DEBUG_EXE="gdb --args "
 elif [ `uname -o` = "Cygwin" ] ; then
-   export BIN_HOME=../../bin/win32-$DMZ_BIN_MODE ;
+   export BIN_HOME=$DMZ_ROOT/bin/win32-$DMZ_BIN_MODE ;
    export DEBUG_EXE="devenv /debugexe "
 else
    echo "Unsupported platform: " `uname`
