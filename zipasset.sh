099649327ce671ec7fe298d738874d693bfd577b
Randall E. Barker <simstorm@mac.com>
2009-07-02 15:48:37 -0700

    Updated zipasset.sh to ignore .DS_Store files

--- zipasset.sh
+++ zipasset.sh
@@ -4,5 +4,5 @@ if [ "$1" = "" ] ; then
   echo "Asset name required"
   exit -1
 else
-   zip -r -9 $1-assets-`date +%y%m%d`.zip ./assets/$1
+   zip -r -9 $1-assets-`date +%y%m%d`.zip ./assets/$1 -x \*.DS_Store
 fi
