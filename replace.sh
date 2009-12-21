632e303efb516fe13d9426118cfb2077672c1cbb
Randall E. Barker <simstorm@mac.com>
2009-12-21 13:32:44 -0800

    Updated replace script to only update files that need it instead of rewriting all files.

--- replace.sh
+++ replace.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-find . \( -name '*.cpp' -o -name '*.h' -o -name '*.lmk' \) | xargs sed -i .sed-replace s/$1/$2/g
+find . \( -name '*.cpp' -o -name '*.h' -o -name '*.lmk' \) | xargs grep -l $1 | xargs sed -i .sed-replace s/$1/$2/g
 find . -name \*.sed-replace | xargs rm
