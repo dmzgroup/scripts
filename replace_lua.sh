4132deb5f0e4f6317a21255311674705c077c463
Randall E. Barker <simstorm@mac.com>
2009-12-21 13:33:27 -0800

    Updated replace_lua script to only update files that need it instead of rewriting all files.

--- replace_lua.sh
+++ replace_lua.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-find . -name '*.lua' | xargs sed -i .sed-replace s/$1/$2/g
+find . -name '*.lua' | xargs grep -l $1 | xargs sed -i .sed-replace s/$1/$2/g
 find . -name \*.sed-replace | xargs rm
