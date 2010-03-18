9562ca9e9c71adaa7e8dfd2e7066d36489cef071
Randall E. Barker <simstorm@mac.com>
2010-03-18 16:55:09 -0700

    added js to extension types that remove the exe bit from.

--- noexe.sh
+++ noexe.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-find . \( -name '*.cpp' -o -name '*.c' -o -name '*.h' -o -name '*.lmk' -o -name '*.xml' -o -name '*.lua' -o -name '*.png' -o -name '*.html' -o -name '*.gif' \) | xargs chmod ugo-x
+find . \( -name '*.cpp' -o -name '*.c' -o -name '*.h' -o -name '*.lmk' -o -name '*.xml' -o -name '*.lua' -o -name '*.png' -o -name '*.html' -o -name '*.gif' -o -name '*.js' \) | xargs chmod ugo-x
